bda31fb922e5d847fa50b8659e5da4d23d7e3c64
odivanmarco <odivanmarcos2@gmail.com>
2022-03-08 09:46:37 -0300

    Renomeando de imobi para concessiOk

--- concessiOK/asgi.py
+++ concessiOK/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for imobi project.
+ASGI config for concessiOK project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imobi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'concessiOK.settings')
 
 application = get_asgi_application()
--- concessiOK/urls.py
+++ concessiOK/urls.py
@@ -1,4 +1,4 @@
-"""imobi URL Configuration
+"""concessiOK URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
