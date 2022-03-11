61f45d38af09f2c9d527dc47c9a2bc2e504d8c7f
odivanmarco <odivanmarcos2@gmail.com>
2022-03-11 10:30:11 -0300

    Mudança no models

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from plataforma.models import  Veiculo, Contato
 
 # Register your models here.
-admin.site.register(Veiculo)
+@admin.register(Veiculo)
+class VeiculoAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'cidade', 'valor', 'telefone',)
+    list_editable = ('valor', 'telefone')
+    list_filter = ('cidade', 'modelo')
+
 admin.site.register(Contato)
 
